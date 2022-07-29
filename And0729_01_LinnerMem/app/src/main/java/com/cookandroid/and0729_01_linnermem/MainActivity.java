12518dd73d693e0d9d42cc03bc67a853f6ed5388
chaemin <w2114@e-mirim.hs.kr>
2022-07-29 10:24:25 +0900

    com

--- And0729_01_LinnerMem/app/src/main/java/com/cookandroid/and0729_01_linnermem/MainActivity.java
+++ And0729_01_LinnerMem/app/src/main/java/com/cookandroid/and0729_01_linnermem/MainActivity.java
@@ -1,16 +1,11 @@
-package com.cookandroid.and0728_05_login;
+package com.cookandroid.and0729_01_linnermem;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-EditText edtid,edtpw;
-Button btn1,btn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
